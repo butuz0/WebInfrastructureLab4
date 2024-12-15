78ddd5c263381f6c55b3dd89af631d7da2fa690d
butuz0 <y.o.oryshchenko@gmail.com>
2024-12-15 14:10:16 +0200

    add mongodb seller api

--- mongodb_app/urls.py
+++ mongodb_app/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('get/', views.get_all_sellers, name='get_all_sellers'),
     path('get/<int:seller_id>/', views.get_seller, name='get_seller'),
     path('create/', views.create_seller, name='create_seller'),
+    path('update/<int:seller_id>/', views.update_seller, name='update_seller'),
+    path('delete/<int:seller_id>/', views.delete_seller, name='delete_seller'),
 ]
--- mongodb_app/views.py
+++ mongodb_app/views.py
@@ -12,8 +12,11 @@ def get_all_sellers(request):
 
 
 def get_seller(request, seller_id):
-    res = Seller.objects.values('seller_id', 'full_name', 'age', 'gender').get(seller_id=seller_id)
-    return JsonResponse(res, status=200)
+    try:
+        res = Seller.objects.values('seller_id', 'full_name', 'age', 'gender').get(seller_id=seller_id)
+        return JsonResponse(res, status=200)
+    except Seller.DoesNotExist:
+        return JsonResponse({'error': 'Data not found'}, status=404)
 
 
 @csrf_exempt
@@ -23,6 +26,10 @@ def create_seller(request):
 
     try:
         data = json.loads(request.body.decode('utf-8'))
+
+        if data['seller_id'] in list(Seller.objects.values_list('seller_id', flat=True)):
+            return JsonResponse({'error': f'Seller id is already used'}, status=400)
+
         seller = Seller.objects.create(
             seller_id=data['seller_id'],
             full_name=data['full_name'],
@@ -42,3 +49,41 @@ def create_seller(request):
             'age': seller.age,
             'gender': seller.gender
         }, status=201)
+
+
+@csrf_exempt
+def update_seller(request, seller_id):
+    if request.method != 'PUT':
+        return JsonResponse({'error': 'Only PUT method is allowed'}, status=405)
+
+    try:
+        seller = Seller.objects.get(seller_id=seller_id)
+        data = json.loads(request.body.decode('utf-8'))
+    except Seller.DoesNotExist:
+        return JsonResponse({'error': 'Data not found'}, status=404)
+    except json.JSONDecodeError:
+        return JsonResponse({'error': 'Invalid JSON'}, status=400)
+    else:
+        for field in data:
+            setattr(seller, field, data[field])
+        seller.save()
+
+        return JsonResponse({
+            'seller_id': seller.seller_id,
+            'full_name': seller.full_name,
+            'age': seller.age,
+            'gender': seller.gender
+        }, status=200)
+
+
+@csrf_exempt
+def delete_seller(request, seller_id):
+    if request.method != 'DELETE':
+        return JsonResponse({'error': 'Only DELETE method is allowed'}, status=405)
+
+    try:
+        Seller.objects.get(seller_id=seller_id).delete()
+    except Seller.DoesNotExist:
+        return JsonResponse({'error': 'Data not found'}, status=404)
+    else:
+        return JsonResponse({'message': 'Deleted successfully'}, status=200)

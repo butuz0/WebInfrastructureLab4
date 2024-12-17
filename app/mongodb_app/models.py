2f762a1b19478461fbd0548087f10f9c58caf8de
v-Wint <tuiooad@gmail.com>
2024-12-17 21:11:55 +0200

    Fixed update api calls for mongodb

--- app/mongodb_app/models.py
+++ app/mongodb_app/models.py
@@ -2,6 +2,7 @@ from djongo import models
 
 
 class Seller(models.Model):
+    _id = models.ObjectIdField()
     seller_id = models.PositiveIntegerField(unique=True, blank=True, null=True)
     full_name = models.CharField(max_length=255)
     age = models.PositiveIntegerField()
@@ -10,8 +11,3 @@ class Seller(models.Model):
     class Meta:
         app_label = 'mongodb_app'
 
-    def save(self, *args, **kwargs):
-        if not self.seller_id:  # If id is not provided, assign a new unique id
-            last_seller = Seller.objects.all().order_by('-seller_id').first()
-            self.seller_id = last_seller.seller_id + 1 if last_seller else 1
-        super(Seller, self).save(*args, **kwargs)
